b1399c6f310bf731c80c524f2bc82e39c4fc436c
Vika <haritont180402@gmail.com>
2024-05-06 11:08:49 +0300

    Создан класс для хранения смайликов

---
+++ app/src/main/java/vika/app/healthy_lifestyle/ui/theme/general/EmojiMap.kt
@@ -0,0 +1,40 @@
+package vika.app.healthy_lifestyle.ui.theme.general
+
+val emojiMap = mapOf(
+    "" to "\uD83E\uDDC3",
+    "Без типа" to "\uD83E\uDDC3",
+
+    "Напиток" to "\uD83E\uDDC3",
+    "Фрукт" to "\uD83C\uDF4F",
+    "Сладкое" to "\uD83C\uDF6D",
+    "Приправа" to "\uD83E\uDDC2",
+    "Алкоголь" to "\uD83C\uDF77",
+    "Дичь" to "\uD83D\uDC17",
+    "Рыба" to "\uD83D\uDC1F",
+    "Орех" to "\uD83C\uDF30",
+    "Ягода" to "\uD83C\uDF47",
+    "Вода" to "\uD83D\uDCA7",
+    "Овощ" to "\uD83E\uDD55",
+    "Мучное" to "\uD83C\uDF5E",
+    "Зелень" to "\uD83E\uDD6C",
+    "Соус" to "\uD83E\uDD6B",
+    "Уксус" to "",
+    "Мясо" to "\uD83C\uDF56",
+    "Субпродукт" to "\uD83D\uDC3D",
+    "Сыр" to "\uD83E\uDDC0",
+    "Боб" to "\uD83E\uDEDB",
+    "Крупа" to "\uD83C\uDF5A",
+    "Гриб" to "\uD83C\uDF44",
+    "Молочное" to "\uD83E\uDD5B",
+    "Масло" to "\uD83E\uDDC8",
+    "Яйцо" to "\uD83E\uDD5A",
+
+    "Виды спорта" to "\uD83E\uDD3C",
+    "Бег" to "\uD83C\uDFC3",
+    "Езда на велосипеде" to "\uD83D\uDEB4",
+    "Кондиционирующее упражнение" to "\uD83D\uDCAA",
+    "Ходьба" to "\uD83D\uDEB6",
+    "Бездействие" to "\uD83E\uDDCE",
+    "Домашняя активность" to "\uD83E\uDDF9",
+    "Садоводство" to "\uD83E\uDEB4"
+)
