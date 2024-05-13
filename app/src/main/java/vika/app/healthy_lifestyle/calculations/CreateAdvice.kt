3e96f48b492a4e423be9eb1e427f5db930e760c1
Vika <haritont180402@gmail.com>
2024-05-13 15:07:17 +0300

    Создан класс для генерации советов

---
+++ app/src/main/java/vika/app/healthy_lifestyle/calculations/CreateAdvice.kt
@@ -0,0 +1,19 @@
+package vika.app.healthy_lifestyle.calculations
+
+import android.content.Context
+import vika.app.healthy_lifestyle.bean.main.PersonalData
+import vika.app.healthy_lifestyle.recommend.RecommendSystem
+
+class CreateAdvice {
+    fun getAdvice():String{
+        return "Совет скоро появится"
+    }
+
+    fun getProductAdvice(context: Context, data: PersonalData, meal: String): String {
+        val advice = "Попробуйте на ".plus(meal).plus(" ")
+
+        val products = RecommendSystem(context, data.target, MealCalc().getIndexMeal(meal)).getProducts(1)
+
+        return advice.plus(products[0].product)
+    }
+}
