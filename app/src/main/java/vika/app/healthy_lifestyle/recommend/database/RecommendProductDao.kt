65e8a3aa56c172db204eac71dc07faea4d108d04
Vika <haritont180402@gmail.com>
2024-05-13 14:36:26 +0300

    Исправлен запрос

--- app/src/main/java/vika/app/healthy_lifestyle/recommend/database/RecommendProductDao.kt
+++ app/src/main/java/vika/app/healthy_lifestyle/recommend/database/RecommendProductDao.kt
@@ -10,9 +10,9 @@ interface RecommendProductDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(product: RecommendProductEntity)
 
-    @Query("UPDATE RecommendProduct SET mark = :mark WHERE product = :name")
+    @Query("UPDATE RecommendProduct SET mark =:mark WHERE product =:name")
     suspend fun updateMark(name: String, mark: Double)
 
-    @Query("SELECT * FROM RecommendProduct WHERE mark >= :mark")
-    suspend fun getRecommendProductList(mark: Double): List<RecommendProductEntity>
+    @Query("SELECT * FROM RecommendProduct WHERE target =:target AND meal =:meal AND mark >=:mark")
+    suspend fun getRecommendProductList(mark: Double, target: Int, meal: Int): List<RecommendProductEntity>
 }
