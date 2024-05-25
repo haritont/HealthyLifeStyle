eedc4ed3f8c526f725a7042142d2f92c83fa6a13
Vika <haritont180402@gmail.com>
2024-05-25 11:27:32 +0300

    Добавлена обработка пустых полей при добавлении

--- app/src/main/java/vika/app/healthy_lifestyle/ui/theme/sport/AddPhysicalExercise.kt
+++ app/src/main/java/vika/app/healthy_lifestyle/ui/theme/sport/AddPhysicalExercise.kt
@@ -1,6 +1,7 @@
 package vika.app.healthy_lifestyle.ui.theme.sport
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +14,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -23,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -32,6 +35,7 @@ import androidx.compose.ui.window.Dialog
 import vika.app.healthy_lifestyle.R
 import vika.app.healthy_lifestyle.activity.sport.SportActivity
 import vika.app.healthy_lifestyle.ui.theme.app.Black
+import vika.app.healthy_lifestyle.ui.theme.app.RedLight
 import vika.app.healthy_lifestyle.ui.theme.general.Dropdown
 import vika.app.healthy_lifestyle.ui.theme.general.TextFieldBlue
 
@@ -61,6 +65,13 @@ fun AddPhysicalExercise(
     )
     val typeState = remember { mutableStateOf(options[0]) }
 
+    var colorName by remember {
+        mutableStateOf(Color.Transparent)
+    }
+
+    var colorMet by remember {
+        mutableStateOf(Color.Transparent)
+    }
 
     if (openDialog) {
         Dialog(
@@ -103,59 +114,82 @@ fun AddPhysicalExercise(
                     }
 
                     Spacer(modifier = Modifier.height(10.dp))
-                    TextFieldBlue(
-                        value = nameState.value,
-                        label = {
-                            Text(
-                                LocalContext.current.getString(R.string.input_name),
-                                style = MaterialTheme.typography.bodySmall
-                            )
-                        },
-                        onValueChange = { newLogin -> nameState.value = newLogin },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                        leadingIcon = {
-                            Image(
-                                painterResource(R.drawable.sport),
-                                contentDescription = null,
-                                modifier = Modifier
-                                    .size(25.dp)
-                            )
-                        }
-                    )
+                    Surface(
+                        shape = RoundedCornerShape(10.dp),
+                        modifier = Modifier
+                            .border(3.dp, colorName, RoundedCornerShape(10.dp))
+                    ) {
+                        TextFieldBlue(
+                            value = nameState.value,
+                            label = {
+                                Text(
+                                    LocalContext.current.getString(R.string.input_name),
+                                    style = MaterialTheme.typography.bodySmall
+                                )
+                            },
+                            onValueChange = { newLogin -> nameState.value = newLogin },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                            leadingIcon = {
+                                Image(
+                                    painterResource(R.drawable.sport),
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(25.dp)
+                                )
+                            }
+                        )
+                    }
 
-                    TextFieldBlue(
-                        value = metState.value,
-                        label = {
-                            Text(
-                                LocalContext.current.getString(R.string.met),
-                                style = MaterialTheme.typography.bodySmall
-                            )
-                        },
-                        onValueChange = { newLogin -> metState.value = newLogin },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                        leadingIcon = {
-                            Image(
-                                painterResource(R.drawable.sport),
-                                contentDescription = null,
-                                modifier = Modifier
-                                    .size(25.dp)
-                            )
-                        }
-                    )
+                    Surface(
+                        shape = RoundedCornerShape(10.dp),
+                        modifier = Modifier
+                            .border(3.dp, colorMet, RoundedCornerShape(10.dp))
+                    ) {
+                        TextFieldBlue(
+                            value = metState.value,
+                            label = {
+                                Text(
+                                    LocalContext.current.getString(R.string.met),
+                                    style = MaterialTheme.typography.bodySmall
+                                )
+                            },
+                            onValueChange = { newLogin -> metState.value = newLogin },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                            leadingIcon = {
+                                Image(
+                                    painterResource(R.drawable.sport),
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(25.dp)
+                                )
+                            }
+                        )
+                    }
 
                     Row(
                         horizontalArrangement = Arrangement.Center
                     ) {
                         TextButton(
                             onClick = {
-                                SportActivity().insertPhysicalExercise(
-                                    context,
-                                    nameState.value,
-                                    metState.value.replace(",", ".").toDouble(),
-                                    typeState.value
-                                )
-                                openDialog = !openDialog
-                                onOpenChange(openDialog)
+                                var check = true
+                                if (nameState.value == ""){
+                                    check = false
+                                    colorName = RedLight
+                                }
+                                if (metState.value == ""){
+                                    check = false
+                                    colorMet = RedLight
+                                }
+                                if (check) {
+                                    SportActivity().insertPhysicalExercise(
+                                        context,
+                                        nameState.value,
+                                        metState.value.replace(",", ".").toDouble(),
+                                        typeState.value
+                                    )
+                                    openDialog = !openDialog
+                                    onOpenChange(openDialog)
+                                }
                             },
                             modifier = Modifier.padding(8.dp),
                         ) {
