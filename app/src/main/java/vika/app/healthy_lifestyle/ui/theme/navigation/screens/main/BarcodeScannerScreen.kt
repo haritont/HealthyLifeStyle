77a1ba23f85dfd1a3e804fe4204731b96aa18a1d
Vika <haritont180402@gmail.com>
2024-05-27 13:30:33 +0300

    Создана страница для сканера штрихкодов

--- app/src/main/java/vika/app/healthy_lifestyle/ui/theme/navigation/screens/main/BarcodeScannerScreen.kt
+++ app/src/main/java/vika/app/healthy_lifestyle/ui/theme/navigation/screens/main/BarcodeScannerScreen.kt
@@ -1,8 +1,72 @@
 package vika.app.healthy_lifestyle.ui.theme.navigation.screens.main
 
+import android.Manifest
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
+
+@Composable
+fun RequestCameraPermission(onPermissionGranted: () -> Unit) {
+    val context = LocalContext.current
+    val permissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            onPermissionGranted()
+        } else {
+            Toast.makeText(context, "Camera permission is required", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        permissionLauncher.launch(Manifest.permission.CAMERA)
+    }
+}
 
 @Composable
 fun BarcodeScannerScreen() {
+    var hasCameraPermission by remember { mutableStateOf(false) }
+    var text by remember {
+        mutableStateOf("")
+    }
+    Text(text = text)
 
+    RequestCameraPermission {
+        hasCameraPermission = true
+    }
+
+    val cameraPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        hasCameraPermission = isGranted
+    }
+
+    LaunchedEffect(Unit) {
+        cameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+    }
+
+    val barcodeLauncher = rememberLauncherForActivityResult(
+        contract = ScanContract()
+    ) { result ->
+        if (result.contents != null) {
+            val scannedContent = result.contents
+            text = scannedContent
+        }
+    }
+
+    if (hasCameraPermission) {
+        LaunchedEffect(Unit) {
+            barcodeLauncher.launch(ScanOptions())
+        }
+    }
 }
