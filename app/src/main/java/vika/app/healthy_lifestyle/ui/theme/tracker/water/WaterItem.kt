f0b2954620fac845ff8b8cd19778de8e77d31546
Vika <haritont180402@gmail.com>
2024-06-06 12:30:59 +0300

    Изменен с использованием LazyVerticalGrid

--- app/src/main/java/vika/app/healthy_lifestyle/ui/theme/tracker/water/WaterItem.kt
+++ app/src/main/java/vika/app/healthy_lifestyle/ui/theme/tracker/water/WaterItem.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -17,23 +16,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieClipSpec
 import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
-import com.airbnb.lottie.compose.rememberLottieAnimatable
 import com.airbnb.lottie.compose.rememberLottieComposition
-import vika.app.healthy_lifestyle.R
 import vika.app.healthy_lifestyle.activity.main.MainActivity
 
 @Composable
 fun WaterItem(
-    value: Int,
-    onAddValue: (Int) -> Unit
+    water: Water
 ) {
     val context = LocalContext.current
     val composition by rememberLottieComposition(
-        spec = LottieCompositionSpec.Asset("water.json")
+        spec = if (water.isStart) {
+            LottieCompositionSpec.Asset("water_add.json")
+        } else {
+            LottieCompositionSpec.Asset("water.json")
+        }
     )
     var isPlaying by remember { mutableStateOf(false) }
     val progress by animateLottieCompositionAsState(
@@ -41,36 +39,34 @@ fun WaterItem(
         isPlaying = isPlaying
     )
 
-    LaunchedEffect(key1 = progress){
-        if (progress == 0f){
+    LaunchedEffect(key1 = progress) {
+        if (progress == 0f) {
             isPlaying = true
         }
-        if (progress == 1f){
+        if (progress == 1f) {
             isPlaying = false
         }
     }
 
-    Column (
+    Column(
         verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.padding(10.dp)
-    ){
+    ) {
         LottieAnimation(
             composition = composition,
             modifier = Modifier
                 .clickable(
                     onClick = {
-                        isPlaying = true
-                        onAddValue(value)
-                        MainActivity().onClickWater(context, value)
+                        if (water.isStart) {
+                            isPlaying = true
+                            water.onAddValue(water.value)
+                            MainActivity().onClickWater(context, water.value)
+                        }
                     }
                 )
                 .size(40.dp),
-            progress = {progress}
-        )
-
-        Text(
-            text = value.toString().plus(LocalContext.current.getString(R.string.milliliter_abbreviation))
+            progress = { progress }
         )
     }
 }
--- app/src/main/java/vika/app/healthy_lifestyle/ui/theme/tracker/water/WaterTracker.kt
+++ app/src/main/java/vika/app/healthy_lifestyle/ui/theme/tracker/water/WaterTracker.kt
@@ -5,13 +5,20 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -21,28 +28,51 @@ import androidx.compose.ui.unit.dp
 import vika.app.healthy_lifestyle.R
 import vika.app.healthy_lifestyle.ui.theme.app.Blue
 import vika.app.healthy_lifestyle.ui.theme.app.BlueLight
-import vika.app.healthy_lifestyle.ui.theme.app.White
 
 @Composable
 fun WaterTracker(
     currentValue: Int
 ) {
-    var currentValueState by remember{ mutableIntStateOf(currentValue) }
+    var currentValueState by remember { mutableIntStateOf(currentValue) }
+    val waters = remember { mutableStateListOf<Water>() }
+
+    if (waters.isEmpty()) {
+        waters.add(
+            Water(
+                value = 100,
+                onAddValue = { value ->
+                    currentValueState += value
+                    waters.add(Water(value = 100, onAddValue = {}, isStart = false))
+                },
+                isStart = true
+            )
+        )
+        val currentWaters = currentValue / 100
+
+        for (count in 1..currentWaters) {
+            waters.add(Water(value = 100, onAddValue = {}, isStart = false))
+        }
+    }
+
     Column(
-        modifier = Modifier.border(
-            width = 4.dp,
-            color = Blue,
-            shape = RoundedCornerShape(10.dp)
-        ).padding(15.dp),
+        modifier = Modifier
+            .border(
+                width = 4.dp,
+                color = Blue,
+                shape = RoundedCornerShape(10.dp)
+            )
+            .padding(15.dp)
+            .size(250.dp),
         verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row(
-            modifier = Modifier.background(
-                color = BlueLight,
-                shape = RoundedCornerShape(10.dp)
-            ).
-        padding(10.dp),
+            modifier = Modifier
+                .background(
+                    color = BlueLight,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .padding(10.dp),
             horizontalArrangement = Arrangement.SpaceAround,
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -52,41 +82,18 @@ fun WaterTracker(
                     .plus(LocalContext.current.getString(R.string.milliliter_abbreviation))
             )
         }
+        
+        Spacer(modifier = Modifier.height(10.dp))
 
-        Row(
-            modifier = Modifier.background(
-                White
-            ),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 60.dp),
+            modifier = Modifier.fillMaxSize()
         ) {
-            WaterItem(50,
-                onAddValue = {value -> currentValueState += value}
-            )
-            WaterItem(100,
-                onAddValue = {value -> currentValueState += value}
-            )
-            WaterItem(250,
-                onAddValue = {value -> currentValueState += value}
-            )
-        }
-
-        Row(
-            modifier = Modifier.background(
-                White
-            ),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            WaterItem(300,
-                onAddValue = {value -> currentValueState += value}
-            )
-            WaterItem(750,
-                onAddValue = {value -> currentValueState += value}
-            )
-            WaterItem(1000,
-                onAddValue = {value -> currentValueState += value}
-            )
+            items(waters) { water ->
+                WaterItem(water)
+            }
         }
     }
 }
+
+data class Water(val value: Int, val onAddValue: (Int) -> Unit, val isStart: Boolean)
