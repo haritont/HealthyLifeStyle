0802a15244c429283b56b83743ba758b49cb3539
Vika <haritont180402@gmail.com>
2024-06-12 19:29:45 +0300

    Обновлена версия

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "vika.app.healthy_lifestyle"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "6"
+        versionCode = 7
+        versionName = "7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
