9cb7847272a40a22c74e7ab8048ac30f8d845dc4
Vika <haritont180402@gmail.com>
2024-06-13 15:48:33 +0300

    Добавлены ресурсы

---
+++ app/src/main/java/vika/app/healthy_lifestyle/ui/theme/app/Color.kt
@@ -0,0 +1,23 @@
+package vika.app.healthy_lifestyle.ui.theme.app
+
+import androidx.compose.ui.graphics.Color
+
+val Blue = Color(0xFF49CCFD)
+val Blue2 = Color(0xFF26C4FF)
+val BlueDark = Color(0xFF00BAFF)
+val BlueLight = Color(0x8026C4FF)
+val BlueUltraLight = Color(0xFFCBF1FF)
+val BlueLight2 = Color(0x8026C4FF)
+
+val PurpleLight = Color(0xFFA69DFF)
+val Purple = Color(0xFF5D4DFF)
+
+val Red = Color(0xFFF53B2D)
+val RedLight = Color(0xB0F14F42)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+val Green = Color(0xFF4CAF50)
+val Yellow = Color(0xFFFFDE07)
+val Orange = Color(0xFFFF8400)
