0c516fc8089e6160f874f3e1acf6003e05096516
Vika <haritont180402@gmail.com>
2024-06-13 14:40:21 +0300

    Обновление цели после обновления личных данных

--- app/src/main/java/vika/app/healthy_lifestyle/activity/main/ProfileActivity.kt
+++ app/src/main/java/vika/app/healthy_lifestyle/activity/main/ProfileActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import vika.app.healthy_lifestyle.base.data.repository.main.PersonalDataRepository
+import vika.app.healthy_lifestyle.base.data.repository.main.RecordRepository
 import vika.app.healthy_lifestyle.bean.main.PersonalData
+import vika.app.healthy_lifestyle.bean.main.Record
+import vika.app.healthy_lifestyle.calculation.DateToday
+import vika.app.healthy_lifestyle.calculation.PersonalTarget
 import vika.app.healthy_lifestyle.ui.theme.app.Healthy_LifestyleTheme
 
 class ProfileActivity : ComponentActivity(){
@@ -23,5 +27,20 @@ class ProfileActivity : ComponentActivity(){
 
     fun insertPersonalData(context: Context, personalData: PersonalData) {
         PersonalDataRepository(context).insertPersonalData(personalData)
+        val newTarget = PersonalTarget()
+        newTarget.count(personalData, context)
+        val record = RecordRepository(context).getRecordByDate(DateToday().getToday())!!
+        RecordRepository(this).insertRecord(
+            Record(
+                id = record.id,
+                date = DateToday().getToday(), targetKilocalories = newTarget.kilocalories,
+                targetProteins = newTarget.proteins, targetFats = newTarget.fats,
+                targetCarbohydrates = newTarget.carbohydrates, targetWater = newTarget.water,
+                progressKilocalories = record.progressKilocalories, progressProteins = record.progressProteins,
+                progressFats = record.progressFats, progressCarbohydrates = record.progressCarbohydrates,
+                progressWater = record.progressWater, progressSteps = record.progressSteps,
+                burnedKilocalories = record.burnedKilocalories
+            )
+        )
     }
 }
