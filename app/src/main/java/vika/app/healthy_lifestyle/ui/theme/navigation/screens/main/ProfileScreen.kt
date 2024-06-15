c41eb8d8d5e69268f2efc0244cd70e4523b77e9c
Vika <haritont180402@gmail.com>
2024-06-15 10:54:49 +0300

    Добавлена регистрация

--- app/src/main/java/vika/app/healthy_lifestyle/ui/theme/navigation/screens/main/ProfileScreen.kt
+++ app/src/main/java/vika/app/healthy_lifestyle/ui/theme/navigation/screens/main/ProfileScreen.kt
@@ -1,5 +1,7 @@
 package vika.app.healthy_lifestyle.ui.theme.navigation.screens.main
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,20 +18,31 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import vika.app.healthy_lifestyle.R
+import vika.app.healthy_lifestyle.activity.TextFieldLogin
+import vika.app.healthy_lifestyle.activity.TextFieldPassword
 import vika.app.healthy_lifestyle.activity.main.ProfileActivity
+import vika.app.healthy_lifestyle.base.data.repository.food.IngredientRepository
+import vika.app.healthy_lifestyle.base.data.repository.main.TypeRepository
 import vika.app.healthy_lifestyle.base.data.repository.main.WeightRepository
+import vika.app.healthy_lifestyle.base.data.repository.mood.EmotionRepository
+import vika.app.healthy_lifestyle.base.data.repository.mood.HabitRepository
+import vika.app.healthy_lifestyle.base.data.repository.sport.PhysicalExerciseRepository
 import vika.app.healthy_lifestyle.bean.main.PersonalData
 import vika.app.healthy_lifestyle.calculation.DateToday
+import vika.app.healthy_lifestyle.server.api.DefaultApiServiceRepository
 import vika.app.healthy_lifestyle.ui.theme.general.ButtonBlue
 import vika.app.healthy_lifestyle.ui.theme.general.DatePickerWithDialog
 import vika.app.healthy_lifestyle.ui.theme.general.TextFieldBlue
@@ -250,6 +263,84 @@ fun ProfileScreen() {
                     value = weightState.value.toDouble()
                 )
             }
+
+            if (getToken(context) == "local_token"){
+                var loginState by remember { mutableStateOf("") }
+                var passwordState by remember { mutableStateOf("") }
+
+                TextFieldLogin(loginState) { newLogin ->
+                    loginState = newLogin
+                }
+
+                TextFieldPassword(passwordState) { newPassword ->
+                    passwordState = newPassword
+                }
+
+                ButtonBlue(
+                    stringResource(id = R.string.login_to_account),
+                    onClick = {
+                        registration(context, loginState, passwordState, ProfileActivity().getPersonalData(context))
+                    }
+                )
+            }
         }
     }
+}
+
+private fun registration(context: Context, login: String, password: String, personalData: PersonalData) {
+    when (val token = DefaultApiServiceRepository().registration(login, password, personalData)) {
+        null -> {
+            showToast(context, "Ошибка подключения к серверу.")
+        }
+
+        "" -> {
+            showToast(context, "Ошибка регистрации. Такой логин уже существует")
+        }
+
+        else -> {
+            setToken(context, token)
+            showToast(context, "Регисрация успешна. Идет загрузка данных...")
+
+            val service = DefaultApiServiceRepository()
+
+            val ingredients = service.getAllIngredients()
+            for (ingredient in ingredients) {
+                IngredientRepository(context).insertIfNotExists(ingredient)
+                TypeRepository(context).insertIfNotExists(ingredient.type)
+            }
+
+            val physicalExercises = service.getAllPhysicalExercise()
+            for (physicalExercise in physicalExercises) {
+                PhysicalExerciseRepository(context).insertIfNotExists(
+                    physicalExercise
+                )
+                TypeRepository(context).insertIfNotExists(physicalExercise.type)
+            }
+
+            val habits = service.getAllHabits()
+            for (habit in habits) {
+                HabitRepository(context).insertIfNotExists(habit)
+            }
+
+            val emotions = service.getAllEmotions()
+            for (emotion in emotions) {
+                EmotionRepository(context).insertIfNotExists(emotion)
+            }
+
+        }
+    }
+}
+private fun showToast(context:Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
+
+private fun setToken(context: Context, token: String) {
+    val sharedPreferences = context.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+    val editor = sharedPreferences.edit()
+    editor.putString("token", token)
+    editor.apply()
+}
+private fun getToken(context: Context) : String? {
+    val sharedPreferences = context.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+    return sharedPreferences.getString("token", null)
 }
